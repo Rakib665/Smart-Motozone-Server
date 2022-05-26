ee741d7d8a6d72e7b7694c0ef94b2c5dd1f7571c
Rakib665 <islammdr44@gmail.com>
2022-05-27 03:24:28 +0600

    after create for purchase api

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const cors = require('cors');
 const port = process.env.PORT || 5000
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 
@@ -15,21 +15,28 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_Pass}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
-async function run(){
-try{
-await client.connect()
+async function run() {
+  try {
+    await client.connect()
 
-const partsCollection = client.db('motozone-parts').collection('parts')
-app.get('/parts', async (req, res) => {
-    const query = {}
-    const allParts = await partsCollection.find(query).toArray()
-    res.send(allParts)
-  })
+    const partsCollection = client.db('motozone-parts').collection('parts')
+    app.get('/parts', async (req, res) => {
+      const query = {}
+      const allParts = await partsCollection.find(query).toArray()
+      res.send(allParts)
+    })
 
-}
-finally{
+    app.get('/parts/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) }
+      const item = await partsCollection.findOne(query)
+      res.send(item)
+    })
 
-}
+  }
+  finally {
+
+  }
 }
 
 run().catch(console.dir)
@@ -37,9 +44,9 @@ run().catch(console.dir)
 
 
 app.get('/', (req, res) => {
-    res.send('Hello MotoZone!')
-  })
-  
-  app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
+  res.send('Hello MotoZone!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
