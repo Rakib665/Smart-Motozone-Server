3cfbe0ac83bbcf99c3b3c1b517aecceac0378811
Rakib665 <islammdr44@gmail.com>
2022-05-30 00:05:26 +0600

    heroku

--- index.js
+++ index.js
@@ -50,21 +50,7 @@ async function run() {
     const profileCollection = client.db('motozone-parts').collection('profiles')
 
 
-    // app.put('/user/:email', async(req,res)=>{
-    //   const email = req.params.email;
-    //   const user = req.body;
-    //   const filter = { email: email };
-    //   const options = { upsert: true };
-    //   const updateDoc = {
-    //     $set: user,
-    //   };
-    //   const result = await userCollection.updateOne(filter, updateDoc, options);
-    //   const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
-    //   console.log(token)
-    //   res.send({ result, token });
-    // })
-
-    pp.put('/user/:email', async (req, res) => {
+    app.put('/user/:email', async(req,res)=>{
       const email = req.params.email;
       const user = req.body;
       const filter = { email: email };
@@ -100,9 +86,14 @@ async function run() {
       res.send({admin: isAdmin})
     })
 
-    app.get('/parts', async (req, res) => {
-      const query = {}
-      const allParts = await partsCollection.find(query).toArray()
+    // app.get('/parts', async (req, res) => {
+    //   const query = {}
+    //   const allParts = await partsCollection.find(query).toArray()
+    //   res.send(allParts)
+    // })
+
+    app.get('/parts', async(req,res)=>{
+      const allParts = await partsCollection.find().toArray()
       res.send(allParts)
     })
 
